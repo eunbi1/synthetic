f96e4d27d0e20426fe87244dc6123c58f4f63f96
eunbi1 <82250837+eunbi1@users.noreply.github.com>
2023-01-05 21:23:09 +0900

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -108,9 +108,13 @@ def pc_sampler2(score_model,
     return e_L, x_s, trajectory
 
 
-def ode_score_update(score_model, sde, x_s, s, t, h=0.6, clamp=10):
+def ode_score_update(score_model, sde, x_s, y, s, t, h=0.6, clamp=10):
     alpha = sde.alpha
-    score_s = score_model(x_s, s) * torch.pow(sde.marginal_std(s) + 1e-4, -(alpha-1))[:, None]
+    if  y is not None:
+        y = torch.ones((x_s.shape[0],))*y
+
+
+    score_s = score_model(x_s, y,s) * torch.pow(sde.marginal_std(s) + 1e-4, -(alpha-1))[:, None]
 
     lambda_s = sde.marginal_lambda(s)
     lambda_t = sde.marginal_lambda(t)
@@ -142,7 +146,7 @@ def ode_sampler(score_model,
     elif noise_mode == 'gaussian':
         e_L = torch.randn(size =(batch_size, dimension))*np.sqrt(2)
     x_s = e_L
-
+    trajectory = [x_s]
 
     time_steps = torch.linspace(sde.T, 1e-5, num_steps)
 
@@ -153,12 +157,15 @@ def ode_sampler(score_model,
         for t in tqdm.tqdm(time_steps[1:]):
             batch_time_step_t = torch.ones(x_s.shape[0]) * t
 
-            x_s = ode_score_update(score_model, sde, x_s, batch_time_step_s, batch_time_step_t)
+            x_s = ode_score_update(score_model, sde, x_s, y, batch_time_step_s, batch_time_step_t)
+            trajectory.append(x_s)
             batch_time_step_s = batch_time_step_t
+    trajectory = torch.stack(trajectory, dim=1)
 
-    return  e_L, x_s
+    return  e_L, x_s, trajectory
 from sklearn.mixture import GaussianMixture
 import seaborn as sns
+
 def plt_marginal_density(score_model, ax, npts=1000, memory=5000, title="q0(x)", dim=1, LOW=-7, HIGH=7, device='cuda', name='img.png', noise_mode= 'isotropic'):
     if dim == 1:
         yy = torch.from_numpy(np.linspace(-1.5,1.5, 1000)).reshape((-1, 1)).float()
@@ -193,15 +200,84 @@ def plt_marginal_density(score_model, ax, npts=1000, memory=5000, title="q0(x)",
         ax.imshow(px, cmap='inferno')
 from prdc import compute_prdc
 
-import matplotlib.pyplot as plt
-
 def get_cmap(n, name='hsv'):
     '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
     RGB color; the keyword argument name must be a standard mpl colormap name.'''
     return plt.cm.get_cmap(name, n)
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
+
+
+
+
 
 from matplotlib.lines import Line2D
-def runner(alpha,  dataset= "swiss roll",train_max_iter=100,batch_size=9,
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.lines as lines
+import matplotlib.transforms as mtransforms
+import matplotlib.text as mtext
+
+
+class MyLine(lines.Line2D):
+    def __init__(self, *args, **kwargs):
+        # we'll update the position when the line data is set
+        self.text = mtext.Text(0, 0, '')
+        super().__init__(*args, **kwargs)
+
+        # we can't access the label attr until *after* the line is
+        # initiated
+        self.text.set_text(self.get_label())
+
+    def set_figure(self, figure):
+        self.text.set_figure(figure)
+        super().set_figure(figure)
+
+    def set_axes(self, axes):
+        self.text.set_axes(axes)
+        super().set_axes(axes)
+
+    def set_transform(self, transform):
+        # 2 pixel offset
+        texttrans = transform + mtransforms.Affine2D().translate(2, 2)
+        self.text.set_transform(texttrans)
+        super().set_transform(transform)
+
+    def set_data(self, x, y):
+        if len(x):
+            self.text.set_position((x[-1], y[-1]))
+
+        super().set_data(x, y)
+
+    def draw(self, renderer):
+        # draw my label at the end of the line with 2 pixel offset
+        super().draw(renderer)
+        self.text.draw(renderer)
+
+
+
+
+def animation_gif(trajectory):
+    fig, ax = plt.subplots()
+    i=0
+
+    line = MyLine([], [], mfc='red', ms=12, label='line label')
+    def update(i):
+        print(i)
+
+
+        line.set_data(trajectory[0, :i+1, 0], trajectory[0, :i+1, 1])
+
+    graph_ani = FuncAnimation(fig=fig, func=update, frames=10)
+    graph_ani.save('graph_ani.gif', writer='imagemagick',  dpi=100)
+
+    plt.show()
+def runner(alpha,  dataset= "swiss roll",train_max_iter=100,batch_size=1000,
            max_iter=100, noise_mode="isotropic", label_mode = "fDSM",dimension=2 ,ratio=4,
            sampler = 'ode_sampler', mode=2,
            condition = False, y= None):
@@ -215,7 +291,7 @@ def runner(alpha,  dataset= "swiss roll",train_max_iter=100,batch_size=9,
     testset = datasets[2]
     x_min = torch.min(testset).item()
     x_max =torch.max(testset).item()
-    testset = testset.cpu().numpy()
+    testset = testset.detach().cpu().numpy()
 
 
     sde = VPSDE(alpha, beta_min=0.1, beta_max=20,  schedule='cosine')
@@ -240,10 +316,10 @@ def runner(alpha,  dataset= "swiss roll",train_max_iter=100,batch_size=9,
                              show_image=True, noise_mode = noise_mode, label_mode = label_mode,
                              y=y)
         elif sampler == "ode_sampler":
-            e_L, sample = ode_sampler(score_model,
+            e_L, sample, trajectory= ode_sampler(score_model,
                              score_check_steps=[0, 500, 800, 999],
                              batch_size=batch_size,
-                             num_steps=20,
+                             num_steps=50,
                              show_image=True, noise_mode = noise_mode, label_mode = label_mode)
         e_L = e_L.cpu().numpy()
         trajectory = trajectory.cpu().numpy()
@@ -288,29 +364,27 @@ def runner(alpha,  dataset= "swiss roll",train_max_iter=100,batch_size=9,
     variance = np.var(F)
     print(f'Alpha{alpha}, FID:{average_fid}, Variance: {variance} ratio{ratio} predict:{average_ratio:.1f}')
     if dimension >1:
-        X = sample[:,0]
-        Y = sample[:,1]
+        X = sample[:10,0]
+        Y = sample[:10,1]
         fig = plt.figure()
         ax = fig.add_subplot(111)
         plt.scatter(X, Y, s=1, label = f' ratio{ratio},Alpha{alpha} predict:{average_ratio:.1f}var:{variance:.4f}FID:{average_fid:.4f}distance: {total_distance/3/(e+1):.4f}')
-
-
-
-
+        trajectory = trajectory[:10,:,:]
         plt.scatter(trajectory[:,0,0], trajectory[:,0,1], s=1, c='red',alpha=0.5)
         plt.scatter(trajectory[:,-1, 0], trajectory[:,-1, 1], s=1, c='blue', alpha=0.5)
         cmap = get_cmap(len(trajectory))
         print('shape', np.shape(trajectory))
-        for i in tqdm.tqdm(range(int(batch_size))):
+        for i in tqdm.tqdm(range(len(trajectory))):
             #c = cmap(i))
-            ax.add_line(Line2D(trajectory[i, :, 0], trajectory[i, :, 1], alpha=0.1,c = cmap(i)))
-            plt.scatter(trajectory[i, :, 0], trajectory[i, :, 1], s=0.02, c='black', alpha=0.5)
-        #
+            ax.add_line(Line2D(trajectory[i, :, 0], trajectory[i, :, 1], alpha=0.1, c = cmap(i)))
+        #     plt.scatter(trajectory[i, :, 0], trajectory[i, :, 1], s=0.02, c='black', alpha=0.5)
+        # #
         # plt.scatter(e_L[:, 0], e_L[:, 1], s=1, alpha=0.1, label=f'pre{average_precision:.4f}, rec{average_recall:.4f}, den{average_density:.4f}, cov{average_coverage:.4f}', c='black')
         plt.scatter(testset[:,0], testset[:,1], s=1, label=f'testset', alpha=0.03,c='black')
         plt.xlim(x_min, x_max)
         plt.ylim(x_min, x_max)
         plt.axis('equal')
+
 
         # plt.legend(loc='lower right')
 
@@ -330,9 +404,6 @@ def runner(alpha,  dataset= "swiss roll",train_max_iter=100,batch_size=9,
 
     plt.savefig(name)
 
-    plt.cla()
-    plt.clf()
-    #
     # name ='density_'+ str(dimension)+ label_mode+''+noise_mode+dataset+str(alpha)+'.png'
     # name = os.path.join(dir_path, name)
     # plt_marginal_density(score_model, ax, name=name, dim =dimension,noise_mode=noise_mode)
@@ -396,34 +467,34 @@ def sample(alpha, path="/scratch/private/eunbiyoon/LIM-synthetic-data/10_fDSM__i
 #     for dataset in ["mixture of gaussian"]:
 #         runner(alpha, dataset=dataset, train_max_iter= train_max_iter,dimension = dimension,
 #                max_iter=max_iter, noise_mode=noise_mode, label_mode=label_mode)
-train_max_iter=1000
-max_iter=1
-condition = True
-y=1
-fraction = [10]
-for dimension in [2]:
-#,(1.8, 'fDSM', 'independent'), (1.8, 'fDSM', 'isotropic'),
- for alpha, label_mode, noise_mode in [(2.0, 'fDSM', 'isotropic'), (1.5, 'fDSM', 'isotropic')]:
-  for dataset in ["mixture of gaussian"]:
-    F = []
-    for ratio in fraction:
-     if dataset== 'mog':
-         dimension=1
-     elif dataset == 'checkerboard':
-         dimension=2
-     else:
-         pass
-     fid = runner(alpha, dataset=dataset, train_max_iter=train_max_iter, dimension=dimension,
-                      max_iter=max_iter, noise_mode=noise_mode, label_mode=label_mode, ratio=ratio,
-                  sampler = "pc_sampler2", condition=condition, y= y)
+# train_max_iter=1
+# max_iter=1
+# condition = False
+# y=None
+# fraction = [5]
+# for dimension in [2]:
+# #,(1.8, 'fDSM', 'independent'), (1.8, 'fDSM', 'isotropic'),
+#  for alpha, label_mode, noise_mode in [(2.0, 'fDSM', 'isotropic'), (1.5, 'fDSM', 'isotropic')]:
+#   for dataset in ["mixture of gaussian"]:
+#     F = []
+#     for ratio in fraction:
+#      if dataset== 'mog':
+#          dimension=1
+#      elif dataset == 'checkerboard':
+#          dimension=2
+#      else:
+#          pass
+#      fid = runner(alpha, dataset=dataset, train_max_iter=train_max_iter, dimension=dimension,
+#                       max_iter=max_iter, noise_mode=noise_mode, label_mode=label_mode, ratio=ratio,
+#                   sampler = "ode_sampler")
+#
+#      name = 'fid' + str(dimension) + label_mode + '' + noise_mode + dataset + str(alpha) + '.png'
+#      if condition == True:
+#          name = 'condition'+name
+#      name = os.path.join("/scratch/private/eunbiyoon/LIM-synthetic-data/dataset", name)
+#
 
-     name = 'fid' + str(dimension) + label_mode + '' + noise_mode + dataset + str(alpha) + '.png'
-     if condition == True:
-         name = 'condition'+name
-     name = os.path.join("/scratch/private/eunbiyoon/LIM-synthetic-data/dataset", name)
-     plt.savefig(name, dpi=500)
-
-train_max_iter=1000
+train_max_iter=100
 max_iter=1
 condition = False
 y=None
